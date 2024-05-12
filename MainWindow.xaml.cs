ba0f3b7348fb8a22e935b46beccb072428eeaea4
Andrey Tikhanov <andrei.tihanov@mail.ru>
2024-05-13 02:05:48 +0400

    Create assistant

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Assistant.Model;
+using Assistant.View;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assistant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static string pathNote = "notes.json";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.NavigationService.Navigate(new MainPage());
+        }
+    }
+}
---
+++ Model/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assistant.Model
+{
+    internal class Note
+    {
+        public string Title {  get; set; }
+        public string Description { get; set; }
+        public Note() { }
+        public Note(string title, string description) {  Title = title; Description = description; }   
+    }
+}
